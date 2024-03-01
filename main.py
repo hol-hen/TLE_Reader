e063ae1e5fe0efac824cbd26344495e72be8bc46
Holly Wilson <hollywilson@iinet.net.au>
2024-03-01 16:00:34 +1100

    added math functions to calculate orbital parameters

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ import matplotlib as mpl
 
 
 # Constants
-
+G = 6.67408e-11   #Universal gravitational constant
+mu = 3.986004418e14 #Earth gravitational constant with m^3
+mu2 = 3.986004418e5 #Earth grav constant with km^3
+re = 6378.1370 #Earth radius km
+m_earth = 5.972e24  #Mass of Earth
 
 
 """
@@ -53,61 +57,115 @@ class TLE:
     Functions
     
     """
-    #def compute_year_launched(self): 
+    def compute_year_launched(self): 
+        
+        #add 1900 for years in 1900s (no launches before 1961)
+        if self._year > 60:
+            self.year_launched = self._year + 1900
+        
+        #add 2000 for years in 2000s
+        elif self._year <= 60:
+            self.year_launched = self._year + 2000 
 
     def get_year_launched(self): 
         self.compute_year_launched()
         return self.year_launched
     
-    #def compute_period(self):
+    #calculate orbital period in seconds
+    def compute_period(self):
+        self.period = 2*pi/self.n 
     
     def get_period(self):
         self.compute_period()
         return self.period
     
-    #def compute_a(self):
+    #calculate semi major axis in km
+    def compute_a(self):
+        self.compute_period()
+        self.a = (self.period**2 * mu / (4 * pi**2) )**(1/3)/1000 
     
     def get_a(self):
         self.compute_a()
         return self.a
     
-    #def compute_altitude(self)
+    def compute_altitude(self):
+        self.compute_a()
+        self.altitude = self.a - re 
 
     def get_altitude(self):
         self.compute_altitude()
         return self.altitude
         
-    #def compute_orbit_type(self):   
+    def compute_orbit_type(self):
+        self.compute_altitude()
+
+        #label orbit type depending on altitude
+        if self.altitude < 2000:
+            self.orbit_type = 'LEO (Low Earth Orbit)' 
+        elif self.altitude >= 2000 and self.altitude < 35700:
+            self.orbit_type = 'MEO (Medium Earth Orbit)'
+        elif self.altitude >= 35700 and self.altitude <= 35820:
+            self.orbit_type = 'GEO (Geostationary Orbit)'
+        elif self.altitude < 35820:
+            self.orbit_type = 'HEO (High Earth Orbit)'  
 
     def get_orbit_type(self):
         self.compute_orbit_type()
         return self.orbit_type
     
-    #def compute_orbits_per_day(self):
+    def compute_orbits_per_day(self):
+        self.compute_period()
+        self.orbits_per_day = 24*60*60/self.period
     
     def get_orbits_per_day(self):
         self.compute_orbits_per_day()
         return self.orbits_per_day
     
-    #def compute_speed(self):
+    def compute_speed(self):
+        self.compute_a()
+        self.speed = sqrt(G*m_earth/(self.a*1000))/1000
     
     def get_speed(self):
         self.compute_speed()
         return self.speed
     
-    #def compute_theta(self):
+    def compute_theta(self):
+        self.E = kepler_E(self.e, self.M)
+        self.theta = acos( (cos(self.E) - self.e) / (1 - self.e*cos(self.E)) )
     
     def get_theta(self):
         self.compute_theta()
         return self.theta
     
-    #def compute_r(self): 
+    def compute_r(self):
+        self.compute_a()
+        self.compute_theta()
+        
+        self.r_0 = self.a*(1-self.e**2)/(1+self.e*cos(self.theta)) #calculate position at time of TLE
+
+        #calculate x,y,z components
+        self.x = (cos(self.RAAN) * cos(self.w) - sin(self.RAAN) * sin(self.w) * cos(self.i)) * self.r_0 * cos(self.theta) + (-cos(self.RAAN) * sin(self.w) - sin(self.RAAN) * cos(self.w) * cos(self.i)) * self.r_0 * sin(self.theta)
+        self.y = (sin(self.RAAN) * cos(self.w) + cos(self.RAAN) * sin(self.w) * cos(self.i)) *self. r_0 * cos(self.theta) + (-sin(self.RAAN) * sin(self.w) + cos(self.RAAN) * cos(self.w) * cos(self.i)) * self.r_0 * sin(self.theta)
+        self.z = (sin(self.w) * sin(self.i)) * self.r_0 * cos(self.theta) + (cos(self.w) * sin(self.i)) * self.r_0 * sin(self.theta)
+        
+        #store as position vector
+        self.r = np.array([self.x *1000,self.y*1000,self.z*1000]) 
 
     def get_r(self):
         self.compute_r()
         return self.r
 
-    #def compute_v(self):
+    def compute_v(self):
+        self.compute_a()
+        self.compute_theta()
+
+        #calculate x,y,z components of velocity
+        self.vx = (cos(self.RAAN) * cos(self.w) - sin(self.RAAN) * sin(self.w) * cos(self.i)) * (-(mu2/(self.a*(1-self.e **2)))**(1/2) * sin(self.theta)) + (-cos(self.RAAN) * sin(self.w) - sin(self.RAAN) * cos(self.w) * cos(self.i)) * (mu2/(self.a*(1-self.e **2)))**(1/2) * (self.e + cos(self.theta))
+        self.vy = (sin(self.RAAN) * cos(self.w) + cos(self.RAAN) * sin(self.w) * cos(self.i)) * (-(mu2/(self.a*(1-self.e **2)))**(1/2) * sin(self.theta)) + (-sin(self.RAAN) * sin(self.w) + cos(self.RAAN) * cos(self.w) * cos(self.i)) * (mu2/(self.a*(1-self.e **2)))**(1/2) * (self.e + cos(self.theta))
+        self.vz = (sin(self.w) * sin(self.i)) * (-(mu2/(self.a*(1-self.e **2)))**(1/2) * sin(self.theta)) + (cos(self.w) * sin(self.i)) * (mu2/(self.a*(1-self.e **2)))**(1/2) * (self.e + cos(self.theta))
+        
+        #store as velocity vector
+        self.v = np.array([self.vx*1000,self.vy*1000,self.vz*1000]) 
     
     def get_v(self):
         self.compute_v()
